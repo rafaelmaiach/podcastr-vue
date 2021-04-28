77611bded638573a648a0e45f0f6fb67c6c09fbf
rafaelmaiach <rafael.chieregatto@hotmail.com>
2021-04-27 21:41:09 -0300

    feat: creates Header component

---
+++ components/atoms/Header.vue
@@ -0,0 +1,22 @@
+<template>
+	<header class="flex justify-between items-center bg-white h-24 px-16 py-8 border-b border-gray-200">
+		<div class="flex items-center">
+			<img src="icons/logo.svg" alt="Podcastr Vue Logo">
+			<p class="ml-8 py-1 pl-8 border-l border-gray-200">The best to you hear, always</p>
+		</div>
+		<span class="capitalize">{{ date }}</span>
+	</header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import formatDate from '~/utils/date';
+
+export default defineComponent({
+	setup() {
+		return {
+			date: formatDate(new Date(), 'EEEEEE, d MMMM'),
+		};
+	},
+});
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,42 @@
 <template>
 	<div class="wrapper">
+		<Header id="header" />
 		<Nuxt id="nuxt" />
+		<div id="player" />
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
 
+import Header from '~/components/atoms/Header.vue';
+
 export default defineComponent({
+	components: {
+		Header,
+	},
 	setup() {},
 });
 </script>
+
+<style>
+.wrapper {
+	display: grid;
+	grid-template-areas:
+		"header player"
+		"nuxt player";
+	min-height: 100vh;
+}
+
+#header {
+	grid-area: header;
+}
+
+#nuxt {
+	grid-area: nuxt;
+}
+
+#player {
+	grid-area: player;
+}
+</style>
